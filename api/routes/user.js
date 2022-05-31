fc74175d28a170f712850a290732aa5d19e6c650
Hamid.hrs <hamid47.hrs@gmail.com>
2022-05-31 20:14:26 +0430

    Change audio import names to get each audio section separately.

--- api/routes/user.js
+++ api/routes/user.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const Audio = require("../models/audio");
 const User = require("../models/user");
 
 const router = express.Router();
@@ -9,15 +8,21 @@ router.post("/audio-survey-form", (req, res, next) => {
     userName: req.body.userName,
     nationality: req.body.nationality,
     age: req.body.age,
-    popMusic: req.body.popMusic,
-    rockMusic: req.body.rockMusic,
-    comment: req.body.comment,
+    angry_1: req.body.angry_1,
+    angry_2: req.body.angry_2,
+    angry_3: req.body.angry_3,
+    joy_1: req.body.joy_1,
+    joy_2: req.body.joy_2,
+    joy_3: req.body.joy_3,
+    sad_1: req.body.sad_1,
+    sad_2: req.body.sad_2,
+    sad_3: req.body.sad_3,
   });
   user
     .save()
     .then(() => {
       res.status(200).json({
-        message: "Done.",
+        message: "Done. Thank You For Participating.",
       });
     })
     .catch((err) => {
